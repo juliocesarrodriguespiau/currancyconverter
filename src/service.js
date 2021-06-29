3a0528e9a8ec24875714ac8c3fa3abe609a43e2d
juliocesarrodriguespiau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-06-29 00:09:28 -0300

    commit

--- src/service.js
+++ src/service.js
@@ -30,32 +30,3 @@ class Convert {
 }
 
 module.exports = new Convert();
-
-/**
- * 
- * https://v6.exchangerate-api.com/v6/22f19facf5e1bfd105a6e2f4/pair/BRL/USD
- */
-// {
-//   "result": "success",
-//   "documentation": "https://www.exchangerate-api.com/docs",
-//   "terms_of_use": "https://www.exchangerate-api.com/terms",
-//   "time_last_update_unix": 1624924802,
-//   "time_last_update_utc": "Tue, 29 Jun 2021 00:00:02 +0000",
-//   "time_next_update_unix": 1625011202,
-//   "time_next_update_utc": "Wed, 30 Jun 2021 00:00:02 +0000",
-//   "base_code": "BRL",
-//   "target_code": "USD",
-//   "conversion_rate": 0.2020
-// }
-
-
-
-
-// class { 1 usd * 4.91 = 4.91}
-
-// class { 10 * 4.91 = 40.91}
-
-// 	"base_code": "EUR",
-// 	"target_code": "GBP",
-// 	"conversion_rate": 0.8412,
-// 	"conversion_result": 5.8884
