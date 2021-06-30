f8743533631215dca70e65bef485dbf6fd25bd87
juliocesarrodriguespiau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-06-30 10:24:21 -0300

    commit validação

--- src/controller.js
+++ src/controller.js
@@ -9,7 +9,9 @@ class Convert {
             //validar o retorno from, to, amount.
             const result = await service.converter(from, to, amount);
 
-            resp.status(200).send({result});
+            const result2 = result.toFixed(2)
+
+            resp.status(200).send({result2});
 
         } catch (error) {
             resp.status(500).send({error: error.message});
